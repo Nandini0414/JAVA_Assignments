b8fd29532f5ea365728c89244111eace75466b89
Nandini0414 <nandinireddi01@gmail.com>
2024-07-17 16:20:18 +0530

    Assignments

---
+++ pyramid.java
@@ -0,0 +1,25 @@
+
+// Number-increasing pyramid
+import java.util.*;
+
+public class pyramid {
+    public static void printPattern(int n)
+    {
+        int i, j;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
+
+
