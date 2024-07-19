dcff934bc847d55b4bf3ddbf037de55f09659185
Nandini0414 <nandinireddi01@gmail.com>
2024-07-19 12:32:42 +0530

    java assignment

---
+++ matrix.java
@@ -0,0 +1,12 @@
+public class matrix {
+    public static void main(String args[]){
+        int arr[][]={{1,2},{3,4}};
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                System.out.print (arr[i][j]+"\t");
+
+            }
+            System.out.println();
+        }
+    }
+}
